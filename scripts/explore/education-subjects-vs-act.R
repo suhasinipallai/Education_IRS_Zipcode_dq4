eee3a6984c6290b0625b8b968fcda5590f8b3b02
taylorperkins <taylorperkins.dev@gmail.com>
2018-01-22 09:32:42 -0600

    Added in some different plots to visualize the relationships in the education data.

---
+++ scripts/explore/education-subjects-vs-act.R
@@ -0,0 +1,82 @@
+#!/usr/bin/env Rscript
+
+library("ggplot2") 
+library("stringr")
+library("reshape2")
+library("car")
+
+education_df_plus_3 <- readRDS('./r-objects/education_df_plus_3.rds')
+
+# Subset the education data to only contain the act composite, math, science, and lit cols
+edu_subjects_act <- education_df_plus_3[, c("ACT_Composite", "mean_math", "mean_science", "mean_literature")]
+
+# Melt the df to be indexed by Act compositea
+md_education <- melt(edu_subjects_act, id="ACT_Composite")
+
+scatterplot(
+  ACT_Composite ~ value | variable, 
+  legend.title = "ACT / Math, Science, Lit",
+  xlab = "Avg Subject Scores",
+  ylab = "ACT Scores",
+  data = md_education, 
+  smoother = FALSE, 
+  grid = FALSE, 
+  frame = FALSE
+)
+
+scatterplot(
+  ACT_Composite ~ value | variable, 
+  legend.title = "ACT / Math, Science, Lit ~ Excluding 0 as Avg",
+  xlab = "Avg Subject Scores",
+  ylab = "ACT Scores",
+  data = md_education %>% filter(value != 0), 
+  smoother = FALSE, 
+  grid = FALSE, 
+  frame = FALSE
+)
+
+# Subset the education data to only contain the act composite, math, science, and lit cols
+edu_act_dropout_vs_graduation <- education_df_plus_3[, c("ACT_Composite", "Dropout", "Graduation")]
+
+# Melt the df to be indexed by Act composite
+md_dropout_graduation_act <- melt(edu_act_dropout_vs_graduation, id="ACT_Composite")
+
+
+# These plots were made to help visualize the relationship between act
+# scores and either dropout or graduation rate
+scatterplot(
+  ACT_Composite ~ value, 
+  legend.title = "ACT / Dropout Pct",
+  xlab = "Dropout Pct",
+  ylab = "ACT Scores",
+  data = md_dropout_graduation_act %>% filter(variable == "Dropout"), 
+  smoother = FALSE, 
+  grid = FALSE, 
+  frame = FALSE
+)
+
+scatterplot(
+  ACT_Composite ~ value, 
+  legend.title = "ACT / Graduation Pct",
+  xlab = "Graduation Pct",
+  ylab = "ACT Scores",
+  data = md_dropout_graduation_act %>% filter(variable == "Graduation"), 
+  smoother = FALSE, 
+  grid = FALSE, 
+  frame = FALSE
+)
+
+# Subset the education data to only contain the act composite, math, science, and lit cols
+edu_act_vs_core_region <- education_df_plus_3[, c("ACT_Composite", "CORE_region")]
+
+# Melt the df to be indexed by Act composite
+md_act_core <- melt(edu_act_vs_core_region, id="ACT_Composite")
+
+par(cex.axis=.5)
+boxplot(
+  ACT_Composite ~ value,
+  data=md_act_core, 
+  main="ACT Scores For CORE Regions", 
+  ylab="ACT Scores",
+  las = 2
+)
