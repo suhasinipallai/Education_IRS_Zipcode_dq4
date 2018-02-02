3f9c9f83bd5580d9eeb2265d8cfed9b34d29bb69
taylorperkins <taylorperkins.dev@gmail.com>
2018-02-02 15:48:32 -0600

    Finished the graphs for the final.

--- scripts/explore/education-demo-by-subject-tp.R
+++ scripts/explore/education-demo-by-subject-tp.R
@@ -3,6 +3,7 @@
 library("ggplot2") 
 library("stringr")
 library("gclus")
+library("dplyr")
 
 education_df_plus_3 <- readRDS('./r-objects/education_df_plus_3.rds')
 
@@ -32,7 +33,7 @@ find_cor <- function(df, col_name, thresh=0.2) {
     
     col_corr <- cor(df[[col]], df[[col_name]])
     
-    if (col_corr > thresh) {
+    if (col_corr > thresh || col_corr < (0 - thresh)) {
       cpairs(
         df[, c(col_name, col)],
         gap=.5,
@@ -46,3 +47,19 @@ find_cor <- function(df, col_name, thresh=0.2) {
 for (demo in c("Pct_Black", "Pct_Hispanic", "Pct_Native_American")) {
   find_cor(df = education_tax_income, col_name = demo, thresh = 0.3)
 }
+
+demo_act <- readRDS('./r-objects/zip_irs_education_df.rds')[, c("Pct_Black", "Pct_Hispanic", "ACT_Composite", "county", "income_per_tax_return")]
+
+# Melt the df to be indexed by Act compositea
+md_demo_act <- melt(demo_act, id=c("ACT_Composite", "county", "income_per_tax_return"))
+
+md_demo_act %>% 
+  ggplot(aes(
+    x = value,
+    y = ACT_Composite,
+    color = variable,
+    size = income_per_tax_return
+  )) + 
+  geom_point() +
+  geom_smooth(method='lm') + 
+  labs(x = "Population Percentage")
